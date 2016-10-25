8be23772eac4ca8cd682e20304d59c86046b7f63
Benedek Kiss <benedek.kiss@prezi.com>
2016-10-26 00:14:37 +0200

    Added requirement for number of dwarfes

--- src/main/scala/models/Player.scala
+++ src/main/scala/models/Player.scala
@@ -6,5 +6,7 @@ import models.player_resources.{Dwarf, DwarfWithoutWeapon}
 import scala.collection.immutable.TreeSet
 
 case class Player(playerBoard: PlayerBoard, dwarves: TreeSet[Dwarf], resourceMap: ResourceMap) {
+  require(2 <= dwarves.size && dwarves.size <= 6, s"Not enough or too many dwarf: ${dwarves.size}")
+
   def this() = this(BasicPlayerBoard(), TreeSet(DwarfWithoutWeapon(1), DwarfWithoutWeapon(2)), Map())
 }
