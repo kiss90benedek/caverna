309fab4204011f132ec50b512c59f9d109804baa
Benedek Kiss <benedek.kiss@prezi.com>
2016-10-26 00:11:34 +0200

    Fixed compile error

--- src/main/scala/models/Player.scala
+++ src/main/scala/models/Player.scala
@@ -6,5 +6,5 @@ import models.player_resources.{Dwarf, DwarfWithoutWeapon}
 import scala.collection.immutable.TreeSet
 
 case class Player(playerBoard: PlayerBoard, dwarves: TreeSet[Dwarf], resourceMap: ResourceMap) {
-  def this() = this(BasicPlayerBoard, TreeSet(DwarfWithoutWeapon(1), DwarfWithoutWeapon(2)), Map())
+  def this() = this(BasicPlayerBoard(), TreeSet(DwarfWithoutWeapon(1), DwarfWithoutWeapon(2)), Map())
 }
